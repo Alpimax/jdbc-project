62aa2e8fc13a976d0097eb0160ecd2bf1812602d
Alpimax <alperrozkann@gmail.com>
2022-11-14 14:30:42 -0500

    some updated

--- src/test/resources/Queries/day1/distinct.sql
+++ src/test/resources/Queries/day1/distinct.sql
@@ -1,6 +1,14 @@
-select * from EMPLOYEES;
-select  FIRST_NAME from EMPLOYEES;
-select  distinct  FIRST_NAME from EMPLOYEES;
+select *
+from EMPLOYEES;
+select FIRST_NAME
+from EMPLOYEES;
+select distinct FIRST_NAME
+from EMPLOYEES;
 
-select  distinct  * from EMPLOYEES;
+select distinct *
+from EMPLOYEES;
+
+select JOB_ID
+from JOBS;
+
 
