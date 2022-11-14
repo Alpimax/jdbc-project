1e6c9e0457b8fd4ad5b5d28319e8d7bb9a6d6448
Alpimax <alperrozkann@gmail.com>
2022-11-14 14:57:57 -0500

    some updated

--- src/test/resources/Queries/day1/where.sql
+++ src/test/resources/Queries/day1/where.sql
@@ -1,2 +1,21 @@
 select *
-from EMPLOYEES;
+from EMPLOYEES
+where FIRST_NAME = 'David';
+
+select FIRST_NAME, LAST_NAME, SALARY
+from EMPLOYEES
+where FIRST_NAME = 'Peter';
+
+select FIRST_NAME, LAST_NAME, SALARY
+from EMPLOYEES
+where FIRST_NAME = 'David'
+  and LAST_NAME = 'Austin';
+
+select FIRST_NAME
+from EMPLOYEES
+where SALARY > 6000;
+
+
+select EMAIL
+from EMPLOYEES
+where SALARY < 6000;
