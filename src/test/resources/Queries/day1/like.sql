3dbf05e37ec9803c4534f267db6c4b4b59d93ce2
Alpimax <alperrozkann@gmail.com>
2022-11-15 08:47:14 -0500

    new updated sql files

--- src/test/resources/Queries/day1/like.sql
+++ src/test/resources/Queries/day1/like.sql
@@ -2,10 +2,25 @@ select *
 from EMPLOYEES;
 
 select *
-from EMPLOYEES  where FIRST_NAME like 'B%';
+from EMPLOYEES
+where FIRST_NAME like 'B%';
 
 select *
-from EMPLOYEES where FIRST_NAME like 'B____';
+from EMPLOYEES
+where FIRST_NAME like 'B____';
 
 select *
-from EMPLOYEES where FIRST_NAME like '%z%';
+from EMPLOYEES
+where FIRST_NAME like '%z%';
+
+select *
+from EMPLOYEES
+where FIRST_NAME like '%e';
+
+
+select *
+from EMPLOYEES
+where JOB_ID like '%IT%';
+
+
+-- display all information
