aff590f542eb55c85601df78e0129688594d8ed4
Alpimax <alperrozkann@gmail.com>
2022-11-18 10:20:18 -0500

    new updated sql files

--- src/test/resources/Queries/homework.sql
+++ src/test/resources/Queries/homework.sql
@@ -24,6 +24,17 @@ from EMPLOYEES
 where salary < (select avg(SALARY) from EMPLOYEES);
 
 
-select * from EMPLOYEES;
-select distinct salary from EMPLOYEES order by salary desc;
+select *
+from EMPLOYEES;
+select distinct salary
+from EMPLOYEES
+order by salary desc;
+
+select min(salary)
+from (select distinct SALARY from EMPLOYEES order by salary desc);
+
+select *
+from EMPLOYEES
+where salary = (select min(SALARY)from (select distinct  SALARY from EMPLOYEES order by salary desc )where rownum <4);
+
 
