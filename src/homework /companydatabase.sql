0fa8637bdf7be85b582e0ae72d2138e9ec79d78b
Alpimax <alperrozkann@gmail.com>
2022-11-18 18:28:37 -0500

    OfficeBranch created...

--- src/homework /companydatabase.sql
+++ src/homework /companydatabase.sql
@@ -18,8 +18,20 @@ CREATE TABLE OfficeBranch
     Mgr_start_date DATE,
     FOREIGN KEY (Mgr_id) references OfficeEmployee (Emp_id) on delete set null
 );
+CREATE TABLE officeClient
+(
+    Client_id   int primary key,
+    Client_name varchar(40),
+    Branch_id   int,
+    FOREIGN KEY (Branch_id) references OFFICEBRANCH (Branch_id) on delete set null
+);
 
 ALTER table OFFICEEMPLOYEE
-add foreign key(BRANCH_ID)
-references OFFICEBRANCH(Branch_id)
-on delete  set null ;
+    add foreign key (BRANCH_ID)
+        references OFFICEBRANCH (Branch_id)
+            on delete set null;
+
+Alter table OfficeEmployee
+    add foreign key (SUPER_ID)
+        references OfficeEmployee (EMP_ID)
+            on delete set null;
