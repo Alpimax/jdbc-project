e12b954c1fa9294f70ff43764fa29e0bd260c157
Alpimax <alperrozkann@gmail.com>
2022-11-18 18:17:33 -0500

    OfficeBranch created...

---
+++ src/homework /companydatabase.sql
@@ -0,0 +1,21 @@
+CREATE TABLE OfficeEmployee
+(
+    Emp_id     int primary key,
+    First_name varchar(40),
+    Last_name  varchar(40),
+    Birth_day  date,
+    Sex        varchar(1),
+    Salary     int,
+    Super_id   int,
+    Branch_id  int
+);
+
+CREATE TABLE OfficeBranch
+(
+    Branch_id      int primary key,
+    Branch_name    VARCHAR(40),
+    Mgr_id         int,
+    Mgr_start_date DATE,
+    FOREIGN KEY (Mgr_id) references OfficeEmployee (Emp_id) on delete set null
+);
+
