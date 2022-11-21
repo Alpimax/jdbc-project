6e61b585ed7325c989887612ad3a465459e01d80
Alpimax <alperrozkann@gmail.com>
2022-11-21 11:58:24 -0500

    OfficeBranch created...

--- src/test/java/com/cydeo/jdbctests/TestOracleConnection.java
+++ src/test/java/com/cydeo/jdbctests/TestOracleConnection.java
@@ -1,4 +1,21 @@
 package com.cydeo.jdbctests;
 
-public class TestOracleConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestOracleConnection  {
+    public static void main(String[] args) throws SQLException {
+        String dbUrl = "jdbc:oracle:thin:@44.211.223.224:1521:XE";
+        String dbUS = "hr";
+        String dbPass = "hr";
+
+        Connection conn = DriverManager.getConnection(dbUrl, dbUS, dbPass);
+
+        Statement statement = conn.createStatement();
+
+
+
+    }
 }
